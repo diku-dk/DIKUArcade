d411bd8b354731d27b532a178cd4cf472f3d10d5
Alexander Christensen <alex_c007@hotmail.com>
2017-11-29 22:11:01 +0100

    SimpleSpaceShuttleGame test project

---
+++ SimpleSpaceShuttle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using DIKUArcade;
+
+// TODO: No game project should ever need to reference OpenTK! (OpenTK.Input...)
+
+namespace SimpleSpaceShuttle {
+    internal class Program {
+        public static void Main(string[] args) {
+            var window = new Window("Simple space-shuttle game", 500, AspectRatio.R1X1);
+            var game = new SpaceShuttleGame(window);
+
+            game.GameLoop();
+        }
+    }
+}
