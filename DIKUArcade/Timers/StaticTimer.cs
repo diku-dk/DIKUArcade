d829b793de9ff425bbfd2e80796e8a94e2bc8759
Alexander Christensen <alex_c007@hotmail.com>
2017-11-29 14:00:11 +0100

    Static timer object

---
+++ DIKUArcade/Timers/StaticTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace DIKUArcade.Timers {
+
+    /// <summary>
+    /// Static timer initialized on engine startup. Can be used for
+    /// animations based on static, discrete time intervals.
+    /// </summary>
+    public class StaticTimer {
+        private static Stopwatch timer;
+
+        static StaticTimer() {
+            StaticTimer.timer = new Stopwatch();
+            StaticTimer.timer.Start();
+        }
+
+        public static double GetCurrentTimeFrame() {
+            return StaticTimer.timer.ElapsedMilliseconds;
+        }
+
+        public static void ResetTimer() {
+            StaticTimer.timer.Restart();
+        }
+    }
+}
