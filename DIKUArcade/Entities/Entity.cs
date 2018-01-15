b8f9f8aec7802c2e3cdb0b3f8adec94071a59eb3
Alexander Christensen <alex_c007@hotmail.com>
2018-01-15 21:51:08 +0100

    Renaming scheme

--- DIKUArcade/Entities/Entity.cs
+++ DIKUArcade/Entities/Entity.cs
@@ -1,79 +1,18 @@
-﻿using DIKUArcade.Math;
+﻿using DIKUArcade.Strategies;
+using DIKUArcade.Graphics;
 
 namespace DIKUArcade.Entities {
-    public class Entity {
-        private bool markedForDeletion;
+    public struct Entity {
+        public Shape Shape { get; set; }
+        public IBaseImage Image { get; set; }
 
-        /// <summary>
-        /// Entity's rotational angle measured in radians.
-        /// </summary>
-        public float Rotation { get; set; }
-
-        /// <summary>
-        /// Basic Entity properties
-        /// </summary>
-        public Vec2F Position;
-        public Vec2F Extent { get; set; }
-
-        public void DeleteEntity() {
-            markedForDeletion = true;
+        public Entity(Shape shape, IBaseImage image) {
+            Shape = shape;
+            Image = image;
         }
 
-        public bool IsDeleted() {
-            return markedForDeletion;
-        }
-
-        public void Scale(float scale) {
-            Extent *= scale;
-        }
-
-        public void Scale(Vec2F scalar) {
-            // This is doing pairwise vector multiplication!
-            Extent *= scalar;
-        }
-
-        public void ScaleX(float scale) {
-            Extent.X *= scale;
-        }
-
-        public void ScaleY(float scale) {
-            Extent.Y *= scale;
-        }
-
-        /// <summary>
-        /// Default Move method which does nothing.
-        /// </summary>
-        public virtual void Move() {
-            return;
-        }
-
-        public void Move(Vec2F mover) {
-            Position += mover;
-        }
-
-        public void MoveX(float move) {
-            Position.X += move;
-        }
-
-        public void MoveY(float move) {
-            Position.Y += move;
-        }
-
-        public void Move(float x, float y) {
-            this.MoveX(x);
-            this.MoveY(y);
-        }
-
-        public void Rotate(float angleRadians) {
-            Rotation += angleRadians;
-        }
-
-        public void SetRotation(float angleRadians) {
-            Rotation = angleRadians;
-        }
-
-        public void MoveToPosition(Vec2F newPosition) {
-            Position = newPosition;
+        public void RenderEntity() {
+            Image.Render(this.Shape);
         }
     }
 }
---
+++ DIKUArcade/Entities/Shape.cs
@@ -0,0 +1,101 @@
+﻿using DIKUArcade.Math;
+
+namespace DIKUArcade.Entities {
+    public class Shape {
+        private bool markedForDeletion;
+
+        /// <summary>
+        /// Shape's rotational angle measured in radians.
+        /// </summary>
+        public float Rotation { get; set; }
+
+        /// <summary>
+        /// Basic Shape properties
+        /// </summary>
+        public Vec2F Position;
+        public Vec2F Extent { get; set; }
+
+        public void DeleteEntity() {
+            markedForDeletion = true;
+        }
+
+        public bool IsDeleted() {
+            return markedForDeletion;
+        }
+
+        public void Scale(float scale) {
+            Extent *= scale;
+        }
+
+        public void Scale(Vec2F scalar) {
+            // This is doing pairwise vector multiplication!
+            Extent *= scalar;
+        }
+
+        public void ScaleX(float scale) {
+            Extent.X *= scale;
+        }
+
+        public void ScaleY(float scale) {
+            Extent.Y *= scale;
+        }
+
+        public void ScaleXFromCenter(float scale) {
+            Position.X = (Position.X + Extent.X / 2.0f) - ((Extent.X / 2.0f) * scale);
+            Extent.X *= scale;
+        }
+
+        public void ScaleYFromCenter(float scale) {
+            Position.Y = (Position.Y + Extent.Y / 2.0f) - (Extent.Y / 2.0f * scale);
+            Extent.Y *= scale;
+        }
+
+        public void ScaleFromCenter(float scale) {
+            ScaleXFromCenter(scale);
+            ScaleYFromCenter(scale);
+        }
+
+        public void ScaleFromCenter(Vec2F scalar) {
+            ScaleXFromCenter(scalar.X);
+            ScaleYFromCenter(scalar.Y);
+        }
+
+        /// <summary>
+        /// Default Move method which does nothing.
+        /// </summary>
+        /// // TODO: If we remove MovementStrategy, this method will no longer be needed?
+        public virtual void Move() {
+            return;
+        }
+
+        public void Move(Vec2F mover) {
+            Position += mover;
+        }
+
+        public void MoveX(float move) {
+            Position.X += move;
+        }
+
+        public void MoveY(float move) {
+            Position.Y += move;
+        }
+
+        public void Move(float x, float y) {
+            this.MoveX(x);
+            this.MoveY(y);
+        }
+
+        public void Rotate(float angleRadians) {
+            Rotation += angleRadians;
+        }
+
+        public void SetRotation(float angleRadians) {
+            Rotation = angleRadians;
+        }
+
+        // TODO: Rename to 'SetPosition' for more consistency
+        public void MoveToPosition(Vec2F newPosition) {
+            Position = newPosition;
+        }
+    }
+}
