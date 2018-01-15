fa3614ec376194b70b74dd69b0ca8c34e35508ee
Alexander Christensen <alex_c007@hotmail.com>
2018-01-15 21:53:05 +0100

    Add static random generator class

---
+++ DIKUArcade/Utilities/RandomGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DIKUArcade.Utilities {
+    public class RandomGenerator {
+        public static System.Random Generator { get; private set; }
+
+        static RandomGenerator() {
+            if (RandomGenerator.Generator == null) {
+                RandomGenerator.Generator = new Random();
+            }
+        }
+    }
+}
