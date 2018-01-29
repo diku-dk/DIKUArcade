07a2779ce55875053060cdf6d509d200c4be6f18
Alexander Christensen <alex_c007@hotmail.com>
2018-01-29 14:42:14 +0100

    Physics skethout for collision

---
+++ DIKUArcade/Physics/CheckCollision.cs
@@ -0,0 +1,14 @@
+﻿using DIKUArcade.Entities;
+
+namespace DIKUArcade.Physics {
+
+    // TODO: Rename to 'Collide'?
+    public class CheckCollision {
+        public static CollisionData Aabb(DynamicShape actor, Shape shape) {
+            var data = new CollisionData();
+
+
+            return data;
+        }
+    }
+}
---
+++ DIKUArcade/Physics/CollisionData.cs
@@ -0,0 +1,23 @@
+﻿using DIKUArcade.Math;
+
+namespace DIKUArcade.Physics {
+    public class CollisionData {
+        /// <summary>
+        /// Indicating whether or not a collision has occured.
+        /// </summary>
+        public bool Collision { get; set; }
+
+        /// <summary>
+        /// This factor should be multiplied onto the actor shape's
+        /// direction vector to get the closest position to the
+        /// incident object.
+        /// </summary>
+        public float DirectionFactor { get; set; }
+
+        /// <summary>
+        /// The surface normal of the incident object, indicating
+        /// from which direction a collision has occured.
+        /// </summary>
+        public Vec2F SurfaceNormal { get; set; } // might sometimes be useful!
+    }
+}
