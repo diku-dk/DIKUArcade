1180356b83b145684199b78fe33ac716d9c9150d
Alexander Christensen <alex_c007@hotmail.com>
2018-02-11 01:26:59 +0100

    Add IGameState interface for structural help

---
+++ DIKUArcade/State/IGameState.cs
@@ -0,0 +1,16 @@
+﻿namespace DIKUArcade.State {
+    public interface IGameState {
+        // TODO: Should we have an `InitializeGameState()` method, so that `ResetGameState` will simply call initialize?
+        void GameLoop();
+        void ResetGameState();
+        void UpdateGameLogic();
+        void RenderState();
+        /// <summary>
+        /// Each state can react to key events, delegated from the host StateMachine.
+        /// </summary>
+        /// <param name="keyAction">Eiter "KEY_PRESS" or "KEY_RELEASE".</param>
+        /// <param name="keyValue">The string key value (see DIKUArcade.Input.KeyTransformer
+        /// for details).</param>
+        void HandleKeyEvent(string keyValue, string keyAction);
+    }
+}
