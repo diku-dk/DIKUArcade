3fa549a97fa9d3508129dd2efa0a685351664125
Alexander Christensen <alex_c007@hotmail.com>
2018-02-11 19:51:40 +0100

    Add working animation class

---
+++ DIKUArcade/Graphics/Animation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DIKUArcade.Entities;
+using DIKUArcade.Timers;
+
+namespace DIKUArcade.Graphics {
+    public class Animation {
+        /// <summary>
+        /// Duration of this animation, in milliseconds
+        /// </summary>
+        public int Duration { get; set; }
+
+        /// <summary>
+        /// Position and Extent of this animation
+        /// </summary>
+        public StationaryShape Shape { get; set; }
+
+        /// <summary>
+        /// ImageStride used for animation
+        /// </summary>
+        public ImageStride Stride { get; set; }
+
+        private readonly double timeOfCreation;
+
+        public Animation() {
+            timeOfCreation = StaticTimer.GetElapsedMilliseconds();
+        }
+
+        /// <summary>
+        /// The animation is still considered active if the specified duration
+        /// in milliseconds has not yet passed.
+        /// </summary>
+        public bool IsActive() {
+            return timeOfCreation + Duration > StaticTimer.GetElapsedMilliseconds();
+        }
+
+        public void RenderAnimation() {
+            Stride.Render(Shape);
+        }
+    }
+}
