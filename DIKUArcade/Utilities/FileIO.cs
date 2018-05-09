646f9aead799a4accb04fb46f7ce36f66b289089
Alexander Christensen <alex_c007@hotmail.com>
2018-05-09 23:33:40 +0200

    Add FileIO platform path module

---
+++ DIKUArcade/Utilities/FileIO.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace DIKUArcade.Utilities {
+    public class FileIO {
+
+        /// <summary>
+        /// Return the platform-specific path of the current project directory
+        /// </summary>
+        /// <returns></returns>
+        public static string GetProjectPath() {
+            // find base path
+            var dir = new DirectoryInfo(Path.GetDirectoryName(
+                System.Reflection.Assembly.GetExecutingAssembly().Location));
+
+            while (dir.Name != "bin") {
+                dir = dir.Parent;
+            }
+            dir = dir.Parent;
+
+            return dir.FullName.ToString();
+        }
+    }
+}
